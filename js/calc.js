0e1a847ffa8197edbecc7542b72bdde8ca627551
shrfrn <shrfrn@gmail.com>
2023-09-26 10:39:28 +0300

    Add support for multi digit operands

--- js/calc.js
+++ js/calc.js
@@ -1,28 +1,29 @@
 'use strict'
 
-var num1, num2
+var num1 = '', num2 = ''
 var operator = ''
+var state = 'num1'
 
 function onNumClick(elCell) {
-    console.log(elCell.innerText)
-    if(!num1) num1 = +elCell.innerText
-    else if (operator) num2 = +elCell.innerText
+    if(state === 'num1') num1 += elCell.innerText
+    else if (state === 'num2') num2 += elCell.innerText
     else return
 }
 
 function onOperatorClick(elCell) {
-    if(!num1 || num2) return
+    if(state !== 'num1') return
     operator = elCell.innerText
+    state = 'num2'
 }
 
 function onCalcRes() {
     if(!num2) return
 
-    var res = num1+ num2
+    var res = +num1 + +num2
     var strCalc = num1 + ' + ' + num2 + ' = ' + res 
     
     console.log(strCalc)
     
-    num1 = num2 = 0
-    operator = ''
+    num1 = num2 = operator = ''
+    state = 'num1'
 }
