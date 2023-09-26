bc6a8d89c53e25edd0432fdc45c148c02df61f94
shrfrn <shrfrn@gmail.com>
2023-09-26 10:17:31 +0300

    Add basic logic to store operands in vars

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,11 @@
 'use strict'
 
+var num1, num2
+var operator = '+'
+
 function onNumClick(elCell) {
     console.log(elCell.innerText)
+    if(!num1) num1 = +elCell.innerText
+    else if (operator) num2 = +elCell.innerText
+    else return
 }
